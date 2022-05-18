0a182c114324625257b83f9673c15f3621ebaa77
Thibaut BARRE DE ST VENANT <t.barredestvenan@NeoSoft.local>
2022-05-18 10:12:51 +0200

    header + todo-list test

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,4 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TodoListComponent } from '../todo-list/todo-list.component';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,7 +12,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [RouterTestingModule],
+      declarations: [ HeaderComponent, TodoListComponent]
     })
     .compileComponents();
   });
@@ -21,5 +26,15 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a app title', () => {
+    const element: HTMLElement = fixture.debugElement.nativeElement;
+    expect(element.querySelector('a')?.textContent).toBe('Overkill Todo App');
+  });
+  it('should have a home link', () => {
+    const href = fixture.debugElement.query(By.css('a')).nativeElement.getAttribute('ng-reflect-router-link');
+    fixture.detectChanges();
+    expect(href).toEqual('/')
   });
 });
