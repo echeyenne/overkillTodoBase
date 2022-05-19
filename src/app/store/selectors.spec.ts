9ad304be470fe2a4f7140c14bc87ba5d97ebad4e
Thibaut BARRE DE ST VENANT <t.barredestvenan@NeoSoft.local>
2022-05-19 17:07:48 +0200

    selector test

--- src/app/store/selectors.spec.ts
+++ src/app/store/selectors.spec.ts
@@ -1,18 +1,38 @@
-import {State} from './reducer';
-import {selectTodos} from './selectors';
+import * as fromReducer from './reducer';
+import {selectTodos, selectSelectedTodo, getCloseTodoModalCreation } from './selectors';
 
 describe('Selectors', () => {
-  const initialState: State = {
-   todos: [
-    { id: 1, title: 'todo in memory 1', description: 'description du todo 1', updated: '2019-01-03', isClosed: false },
-    { id: 2, title: 'todo in memory 2', description: 'description du todo 2', updated: '2021-02-03', isClosed: false },
-    { id: 3, title: 'todo in memory 3', description: 'description du todo 3', updated: '2022-05-02', isClosed: false }
-   ],
-   closeTodoModalCreation: false
-  };
-
+  let initialState: fromReducer.State
+  beforeEach(() => {
+    initialState = fromReducer.initialState;
+  })
   it('should select todos list', () => {
     const result = selectTodos.projector(initialState);
     expect(result).toEqual(initialState.todos);
   });
+  it('should select one todo', () => {
+    initialState = { 
+      todos: 
+      [
+        { id: 1, title: 'aTitle', description:'test description', isClosed: false, updated: '2020-01-02' },
+        { id: 2, title: 'aTitle2', description:'test description2', isClosed: false, updated: '2020-10-01' }
+      ], 
+      closeTodoModalCreation: false 
+    };
+    const firstTodo = initialState.todos.find(todo => todo.id === 1);
+    const result = selectSelectedTodo(1).projector(initialState.todos);
+    expect(result).toEqual(firstTodo);
+  });
+  it('should select one todo', () => {
+    initialState = { 
+      todos: 
+      [
+        { id: 1, title: 'aTitle', description:'test description', isClosed: false, updated: '2020-01-02' },
+        { id: 2, title: 'aTitle2', description:'test description2', isClosed: false, updated: '2020-10-01' }
+      ], 
+      closeTodoModalCreation: false 
+    };
+    const result = getCloseTodoModalCreation.projector(initialState);
+    expect(result).toEqual(initialState.closeTodoModalCreation);
+  })
 });
