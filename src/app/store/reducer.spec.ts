a91dc9136823c4d2ed9d5a95f4a6c6e0b106cfa4
Thibaut BARRE DE ST VENANT <t.barredestvenan@NeoSoft.local>
2022-05-19 14:15:14 +0200

    reducer test

--- src/app/store/reducer.spec.ts
+++ src/app/store/reducer.spec.ts
@@ -1,24 +1,19 @@
 import * as fromReducer from './reducer';
 import { State } from './reducer';
-import { loadTodosSuccess } from './actions';
+import { createTodo, createTodoSuccess, loadTodosSuccess } from './actions';
+import { Todo } from '../models/todo';
 
 describe('Reducer', () => {
-  describe('unknown action', () => {
-    it('should return the default state', () => {
-      const { initialState } = fromReducer;
-      const action = {
-        type: 'Unknown',
+  let initialState: fromReducer.State;
+  describe('loadTodos', () => {
+    beforeEach(() => {
+        initialState = fromReducer.initialState;
+    })
+    it('should have all todos on state', () => {
+      const newState: State = { todos: 
+        [{ id: 1, title: 'aTitle', description:'test description', isClosed: false, updated: '2020-01-02' }], 
+        closeTodoModalCreation: false 
       };
-      const state = fromReducer.todosReducer(initialState, action);
-
-      expect(state).toBe(initialState);
-    });
-  });
-
-  describe('loadTodosSuccess action', () => {
-    it('should retrieve all todos and update the state', () => {
-      const { initialState } = fromReducer;
-      const newState: State = { todos: [{ id: 1, title: 'aTitle', isClosed: false, updated: '2020-01-02' }], closeTodoModalCreation: false };
       const action = loadTodosSuccess({
         todos: [...newState.todos],
       });
@@ -29,4 +24,22 @@ describe('Reducer', () => {
       expect(state).not.toBe(newState);
     });
   });
+
+  describe('createTodo', () => {
+    it('should have a new todo after the creation', () => {
+      const todo: Todo = {id: 1, title: 'aTitle', description:'test description', isClosed: false, updated: '2020-01-02'};
+      const emptySate = { todos: [], closeTodoModalCreation: false}
+      initialState = {...emptySate};
+      const expectedState = {
+        closeTodoModalCreation: true , 
+        todos: [todo]
+      };
+
+      const action = createTodoSuccess({ todo });
+      const state = fromReducer.todosReducer(initialState, action);
+      
+      expect(state).toEqual(expectedState);
+      expect(state).not.toBe(emptySate);
+    })
+  })
 });
